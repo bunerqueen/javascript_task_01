03df42387c5b7449da1aa9dbc6c014e17481720e
bunerqueen <abdullahkhangzk@gmail.com>
2024-02-18 10:51:11 +0500

    Chapter-35:Functions Exercises

---
+++ index.js
@@ -0,0 +1,26 @@
+// Welcome message:
+//  function displayWelcomMessage(){
+//     const  name = prompt ("your name");
+//     alert("welcome," + name );
+//  }
+//  displayWelcomMessage()
+
+
+//Quote Generator:
+// function GenerateQuote(){
+//     const quotes = [
+//         " Hello Ayesha",
+//         " How are you",
+//         " What are doing",
+//         " Are you free"
+//     ];
+//     const randomindex = Math.floor(Math.random() * quotes.length);
+//     console.log(quotes[randomindex]);
+// }
+// console.log(GenerateQuote());
+
+  //Random Number:
+   function generateRAndomNumber(){
+    console.log(Math.floor (Math.random() * 100))
+   }
+   generateRAndomNumber()
